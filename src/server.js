3b69381481edf85d4ac2c0ee8f148cc85eb894b6
Flávio Coutinho <coutinhoflavio20@gmail.com>
2022-02-17 21:24:13 +0100

    add file index.ejs

--- src/server.js
+++ src/server.js
@@ -1,8 +1,39 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 const app = express();
 
+const SoftwareDevelopers = [
+    {
+        name: 'John',
+        company: 'Google',
+        technology: 'Node.js',
+    },
+
+    {
+        name: 'Coutinho',
+        company: 'Amazon',
+        technology: 'Python',
+    },
+
+    {
+        name: 'Bill',
+        company: 'Microsoft',
+        technology: 'TypeScript',
+    },
+]
+
 app.get('/', (req, res) => {
-    res.send('Nodeeeeeeeeeeee🚀')
+
+    const filePath = path.join(__dirname, "index.ejs");
+    ejs.renderFile(filePath, { SoftwareDevelopers }, (err, data) => {
+        if (err) {
+            return res.send('Error reading file')
+        }
+
+        return res.send(data)
+    })
+
 })
 
 app.listen(3000)
